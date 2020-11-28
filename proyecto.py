ef3864f436e134bac8904deebe3e98fa36197a81
tololo90 <72520431+tololo90@users.noreply.github.com>
2020-11-28 03:09:11 -0300

    Add files via upload

--- proyecto.py
+++ proyecto.py
@@ -106,10 +106,126 @@ def inicio_programa():
                                      "Inversion": inversion, "Cuota": cuota,
                                      "Resultado": resultado,
                                      "Ganancia": ganancia})
-                    estadisticas.close()ç
+                    estadisticas.close()
                     print("datos agregados correctamente")
                     break
-
+        if seleccion == 2:
+            while True:
+                try:
+                    accion = int(input("Esta en la seccion de estadisticas\nSi"
+                                       " desea continuar escriba 1\nSi deasea"
+                                       " volver atras escriba 2\n"))
+                    if accion == 1:
+                        pass
+                    if accion == 2:
+                        break
+                    if accion != 1 and accion != 2:
+                        print("Error, vuelva a intentarlo")
+                        continue
+                except:
+                    print("Error, vuelva a intentarlo")
+                    continue
+                try:
+                    liga_total = int(input("Para ver estadisticas totales"
+                                           " presione 1\nPara ver estadisticas"
+                                           " por liga presione 2\n"))
+                    if liga_total == 1:
+                        win = 0
+                        lose = 0
+                        total_invertido = 0
+                        total_ganancia = 0
+                        with open('estadisticas.csv') as csvfile:
+                            data = list(csv.DictReader(csvfile))
+                        inversion = len(data)
+                        ganancia = len(data)
+                        win_lose = len(data)
+                        for i in range(inversion):
+                            row = data[i]
+                            inversion = int(row.get('inversion'))
+                            total_invertido += inversion
+                            ganancia = float(row.get('Ganancia'))
+                            total_ganancia += ganancia
+                            win_lose = str(row.get('Resultado'))
+                            if win_lose == "W":
+                                win += 1
+                            if win_lose == "L":
+                                lose += 1
+                        yield_total = (total_ganancia / total_invertido)*100
+                        total = win + lose
+                        winrate = win / total * 100
+                        print(f"Se han invertido {total_invertido} unidades\n"
+                               "El total de las ganancias es de "
+                              f"{total_ganancia:.4} unidades\nEl yield es del"
+                              f" {yield_total:.4}%\nEl porcentaje de winrate "
+                              f"es de {winrate:.4}%")
+                        break
+                    if liga_total == 2:
+                        with open('estadisticas.csv') as csvfile:
+                            total_ligas = []
+                            data = list(csv.DictReader(csvfile))
+                            liga = len(data)
+                            for i in range(liga):
+                                pais = data[i].get('Liga')
+                                if pais not in total_ligas:
+                                    total_ligas.append(pais)
+                            print("Estas son las ligas que puede elegir\n"
+                                  f"{total_ligas}")
+                            liga_elegida = str(input("Copie exactamente como "
+                                                     "figura arriba, "
+                                                     "respentando la primer "
+                                                     "letra mayuscula, la liga"
+                                                     " que quiera\n"))
+                            csvfile = open('temporal.csv', 'w', newline='')
+                            header = ["Mes", "Liga", "Local", "Visitante",
+                                      "Linea", "inversion", "Cuota",
+                                      "Resultado", "Ganancia"]
+                            writer = csv.DictWriter(csvfile, fieldnames=header)
+                            writer.writeheader()
+                            liga = len(data)
+                            repeticion = 0
+                            for i in range(liga):
+                                row = data[i]
+                                liga = data[i].get('Liga')
+                                if liga == liga_elegida:
+                                    repeticion += 1
+                                    writer.writerow(row)
+                            csvfile.close()
+                            win = 0
+                            lose = 0
+                            total_invertido = 0
+                            total_ganancia = 0
+                            with open('temporal.csv') as csvfile:
+                                data = list(csv.DictReader(csvfile))
+                                inversion = len(data)
+                                ganancia = len(data)
+                                win_lose = len(data)
+                                for i in range(inversion):
+                                    row = data[i]
+                                    inversion = int(row.get('inversion'))
+                                    total_invertido += inversion
+                                    ganancia = float(row.get('Ganancia'))
+                                    total_ganancia += ganancia
+                                    win_lose = str(row.get('Resultado'))
+                                    if win_lose == "W":
+                                        win += 1
+                                    if win_lose == "L":
+                                        lose += 1
+                                yield_total = (total_ganancia /
+                                               total_invertido)*100
+                                total = win + lose
+                                winrate = win / total * 100
+                                print(f"Se han invertido {total_invertido} "
+                                      f"unidades en {liga_elegida}\nEl total "
+                                      f"de las ganancias en {liga_elegida} es "
+                                      f"de {total_ganancia:.4} unidades\nEl "
+                                      f"yield en {liga_elegida} es del "
+                                      f"{yield_total:.4}%\nEl porcentaje de "
+                                      f"winrate en {liga_elegida} es de "
+                                      f"{winrate:.4}%")
+                        break
+                except:
+                    print("Error, vuelva a intentarlo")
+                    continue
         if seleccion == 3:
             print("Programa finalizado")
             break
